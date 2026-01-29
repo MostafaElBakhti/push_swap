dc7d1b641a61bf0056f7ee8b5d43452fa444c94a
Mostafa El Bakhti <mel-bakh@c2r5p4.1337.ma>
2026-01-29 23:54:29 +0100

    starting push swap

--- push_swap.h
+++ push_swap.h
@@ -6,15 +6,17 @@
 /*   By: mel-bakh <mel-bakh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/29 22:10:18 by mel-bakh          #+#    #+#             */
-/*   Updated: 2026/01/29 22:11:34 by mel-bakh         ###   ########.fr       */
+/*   Updated: 2026/01/29 22:14:04 by mel-bakh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef PUSH_SWAP_H
 #define PUSH_SWAP_H
 #include <stdlib.h>
 #include <unistd.h>
 #include <limits.h>
 
 
-#ifndef PUSH_SWAP_H
 
+
+#endif

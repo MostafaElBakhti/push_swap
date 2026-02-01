3ad552cff59479417b11cc9eaec728e6f5316005
Mostafa El Bakhti <mel-bakh@c2r11p5.1337.ma>
2026-02-01 23:20:50 +0100

    practice

--- push_swap.h
+++ push_swap.h
@@ -5,16 +5,29 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: mel-bakh <mel-bakh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2026/01/29 22:10:18 by mel-bakh          #+#    #+#             */
-/*   Updated: 2026/01/29 22:14:04 by mel-bakh         ###   ########.fr       */
+/*   Created: 2026/02/01 20:40:23 by mel-bakh          #+#    #+#             */
+/*   Updated: 2026/02/01 20:41:10 by mel-bakh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PUSH_SWAP_H
-#define PUSH_SWAP_H
+# define PUSH_SWAP_H
+
 #include <stdlib.h>
 #include <unistd.h>
 #include <limits.h>
+
+
+
+typedef struct s_stack
+{
+	int				value;
+	int				pos;
+	int				target_pos;
+	int				cost_a;
+	int				cost_b;
+	struct s_stack	*next;
+}	t_stack;
 
 
 

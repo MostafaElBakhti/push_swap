a36a2c01a54bb22706b3e1829aa086edd0e6240b
mostafa <mustmustapha67@gmail.com>
2026-02-05 21:59:01 +0100

    improve code

--- srcs/parsing/practice_args.c
+++ srcs/parsing/practice_args.c
@@ -14,13 +14,112 @@
 #include <stdlib.h>
 #include <limits.h>
 
+int ft_isspace(char c)
+{
+    return (c == ' ' || (c >= 9 && c <= 13));
+}
+
+int is_valid_number(char *str)
+{
+    int i = 0;
+
+    if (!str || str[0] == '\0')
+        return 0;
+
+    while (ft_isspace(str[i]))
+        i++;
+
+    if (str[i] == '+' || str[i] == '-')
+        i++;
+
+    if (str[i] < '0' || str[i] > '9')
+        return 0;
+
+    while (str[i])
+    {
+        if (str[i] < '0' || str[i] > '9')
+            return 0;
+        i++;
+    }
+    return 1;
+}
+
+long ft_atol(char *str)
+{
+    int i = 0;
+    int sign = 1;
+    long result = 0;
+
+    while (ft_isspace(str[i]))
+        i++;
+
+    if (str[i] == '+' || str[i] == '-')
+    {
+        if (str[i] == '-')
+            sign = -1;
+        i++;
+    }
+
+    while (str[i] >= '0' && str[i] <= '9')
+    {
+        result = result * 10 + (str[i] - '0');
+        i++;
+    }
+    return result * sign;
+}
+
+int is_valid_int(long num)
+{
+    return (num >= INT_MIN && num <= INT_MAX);
+}
+
 int main (int argc , char **argv) 
 {
     int i ; 
-    i = 1 ; 
-    while( i < argc )
+    long num ; 
+    int *numbers ; 
+    int count ; 
+
+    if (argc < 2)
     {
-        printf("%s\n" , argv[i]);
-        i++ ; 
+        printf("Usage: %s <number1> <number2> ...\n", argv[0]);
+        return 1;
     }
+
+    numbers = malloc(sizeof(int) * (argc - 1));
+    if (!numbers)
+    {
+        perror("Failed to allocate memory");
+        return 1;
+    }
+    count = 0 ; 
+    i = 1 ;
+    while ( i < argc)
+    {
+        if (!(is_valid_number(argv[i])))
+        {
+            printf("Invalid number: %s\n", argv[i]);
+            free(numbers);
+            return 1;
+        }
+        num = ft_atol(argv[i]);
+        if (!is_valid_int(num))
+        {
+            printf("Number out of int range: %s\n", argv[i]);
+            free(numbers);
+            return 1;
+        }
+        numbers[count++] = (int)num ;
+        i++;
+    }
+    
+    printf("Successfully parsed %d numbers:\n", count);
+    for (i = 0; i < count; i++)
+        printf("%d ", numbers[i]);
+    printf("\n");
+    
+    free(numbers);
+    return 0;
+
+
 }
--- srcs/parsing/practice_atoi.c
+++ srcs/parsing/practice_atoi.c
@@ -18,36 +18,34 @@
 
 int ft_atoi(char *str)
 {
-    int i = 0 ;
+    // "  -456"
+    int i = 0 ; 
     int sign = 1 ;
-    while ( str[i] == ' ' || (str[i] >= 9 && str[i] <= 13) )
-        i++;
+    int result = 0 ; 
+    while (str[i] == ' ' || str[i] == '\t')
+        i++; 
 
-    if ( str[i] == '-' || str[i] == '+' )
+    if (str[i] == '-' || str[i] == '+')
     {
-        if ( str[i] == '-' )
+        if (str[i] == '-' )
             sign = -1 ;
-        i++;
+        i++; 
     }
 
-    int result = 0 ;
-    while ( str[i] >= '0' && str[i] <= '9' )
+    while (str[i] >= '0' && str[i] <= '9')
     {
-        result = result * 10 + (str[i] - '0') ;
-        printf("Current result: %d\n", result);
+        result = result * 10 + str[i] - '0' ; 
         i++;
     }
     return (result * sign) ;
 
 }
-int main(void)
+
+int main ()
 {
-    printf("Test 1: '%s' -> %d (expected: 42)\n", "42", ft_atoi("42"));
-    printf("Test 2: '%s' -> %d (expected: -42)\n", "-42", ft_atoi("-42"));
-    printf("Test 3: '%s' -> %d (expected: 0)\n", "0", ft_atoi("0"));
-    printf("Test 4: '%s' -> %d (expected: 123)\n", "  123", ft_atoi("  123"));
-    printf("Test 5: '%s' -> %d (expected: -456)\n", "  -456", ft_atoi("  -456"));
-    printf("Test 6: '%s' -> %d (expected: 789)\n", "+789", ft_atoi("+789"));
+  
+    printf("\n------------------------------\n");
+    int test = ft_atoi("2147483648");
+    printf("%d",test  );
     
-    return 0;
 }

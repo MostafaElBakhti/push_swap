2fecbb49438f105cb229b390a962fb9e6fc3ded6
Mostafa El Bakhti <mel-bakh@c2r5p4.1337.ma>
2026-02-18 19:20:00 +0000

    update

--- srcs/sorting/sort_turk.c
+++ srcs/sorting/sort_turk.c
@@ -6,9 +6,37 @@
 /*   By: mel-bakh <mel-bakh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/07 03:00:00 by mel-bakh          #+#    #+#             */
-/*   Updated: 2026/02/18 02:46:19 by mel-bakh         ###   ########.fr       */
+/*   Updated: 2026/02/18 19:11:53 by mel-bakh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/push_swap.h"
 
+int get_abs(int n)
+{
+	if(n < 0)
+		return -n;
+	return n ; 
+}	
+
+t_stack *get_sheapest(t_stack *stack_a)
+{
+	t_stack *current = stack_a ; 
+	t_stack *cheapest = stack_a ; 
+
+	int lowest_cost = INT_MAX ; 
+	int total_cost = 0 ; 
+
+	while (current)
+	{
+		total_cost = get_abs(current->cost_a) + get_abs(current->cost_b) ;
+		if (total_cost  < lowest_cost)	
+		{
+			lowest_cost = total_cost  ; 
+			cheapest = current ; 
+		}
+
+		current  = current->next ; 
+	}
+	return cheapest ; 
+}
--- srcs/sorting/target.c
+++ srcs/sorting/target.c
@@ -6,32 +6,32 @@
 /*   By: mel-bakh <mel-bakh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/12 13:35:16 by mel-bakh          #+#    #+#             */
-/*   Updated: 2026/02/18 02:38:50 by mel-bakh         ###   ########.fr       */
+/*   Updated: 2026/02/18 18:20:27 by mel-bakh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/push_swap.h"
 
-int find_target_b(t_stack *stack_b , int value)
-{
-    t_stack *current = stack_b ;
-    int     clos_larger ; 
-    int     target_pos = 0 ; 
-    clos_larger = INT_MAX ; 
-    while (current)
-    {
-        if ( current->value > value && current->value < clos_larger)
-        {
-            clos_larger = current->value ; 
-            target_pos = current->pos ; 
-        }
-        current = current->next ; 
-    }
-    if ( clos_larger == INT_MAX)
-        target_pos = get_lowest_pos(stack_b) ; 
+// int find_target_b(t_stack *stack_b , int value)
+// {
+//     t_stack *current = stack_b ;
+//     int     clos_larger ; 
+//     int     target_pos = 0 ; 
+//     clos_larger = INT_MAX ; 
+//     while (current)
+//     {
+//         if ( current->value > value && current->value < clos_larger)
+//         {
+//             clos_larger = current->value ; 
+//             target_pos = current->pos ; 
+//         }
+//         current = current->next ; 
+//     }
+//     if ( clos_larger == INT_MAX)
+//         target_pos = get_lowest_pos(stack_b) ; 
     
-    return target_pos ; 
-}   
+//     return target_pos ; 
+// }   
 
 void assign_targets_b(t_stack *stack_a,t_stack *stack_b)
 {
@@ -72,3 +72,31 @@ int	find_target_a(t_stack *stack_a, int value)
 	return (target_pos);
 }
 
+
+
+
+int find_target_b(t_stack *stack_b , int value)
+{
+	int clos_larger ; 
+	int target_pos ;
+
+	clos_larger = INT_MAX ; 
+	target_pos = 0 ; 
+
+	while (stack_b)
+	{
+		if (stack_b->value > value && stack_b->value < clos_larger)
+		{
+			clos_larger = stack_b->value ; 
+			target_pos = stack_b->pos ; 
+		}
+		stack_b = stack_b->next ; 
+	}
+
+	if (clos_larger == INT_MAX)
+	{
+		target_pos = get_lowest_pos(stack_b) ;
+	}
+
+	return target_pos ; 
+}

898b8f3ccc05f7c5c7ee04769dd7181d772841c8
Mostafa El Bakhti <mel-bakh@c2r7p10.1337.ma>
2026-02-21 00:55:00 +0000

    test

--- srcs/parsing/ft_split.c
+++ srcs/parsing/ft_split.c
@@ -6,7 +6,7 @@
 /*   By: mel-bakh <mel-bakh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/20 13:56:36 by mel-bakh          #+#    #+#             */
-/*   Updated: 2026/02/21 00:15:23 by mel-bakh         ###   ########.fr       */
+/*   Updated: 2026/02/21 00:54:02 by mel-bakh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -41,6 +41,41 @@ int	ft_word_len(char *str)
 	return len ;
 }
 
+size_t	ft_strlen(const char *s)
+{
+	size_t	i;
+
+	i = 0;
+	while (*s)
+	{
+		i++;
+		s++;
+	}
+	return (i);
+}
+
+int	ft_strlcpy(char *dst, const char *src, size_t size)
+{
+	size_t	i;
+	size_t	len;
+
+	i = 0;
+	if (size == 0)
+		return (ft_strlen(src));
+	if (size > 0)
+	{
+		while (src[i] && i < size - 1)
+		{
+			dst[i] = src[i];
+			i++;
+		}
+		dst[i] = '\0';
+	}
+	len = 0;
+	while (src[len])
+		len++;
+	return (len);
+}
 
 char **ft_split(char *str)
 {
@@ -71,17 +106,33 @@ char **ft_split(char *str)
 			i++ ; 
 		}
 	}
+	return test ;
 }
 
 
 
 char **get_args(int argc , char **argv)
 {
-	char **args ; 
-	if (argc == 2)
+	// char **args ; 
+	// if (argc == 2)
+	// {
+	// 	args = ft_split(argv[1]) ; 
+	// }
+
+	char **args = (char **)malloc(sizeof(char *) * argc) ;
+	if (!args)
+		return 0 ;
+	
+	for (int i = 0 ; i < argc - 1 ; i++)
 	{
-		args = ft_split(argv[1]) ; 
+		args[i] = (char *)malloc(sizeof(char) * (ft_strlen(argv[i + 1]) + 1)) ;
+		if (!args[i])
+			return 0 ;
+		ft_strlcpy(args[i], argv[i + 1], ft_strlen(argv[i + 1]) + 1) ; 
 	}
+	args[argc - 1] = NULL ;
+	return args ;
+
 }
 // test split
 int main(int argc , char **argv)

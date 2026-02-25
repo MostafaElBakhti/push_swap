28c0cd799d122329561c82cb08c14fe1a288b593
Mostafa El Bakhti <mel-bakh@c2r11p8.1337.ma>
2026-02-25 02:27:23 +0000

    push

--- sort_small.c
+++ sort_small.c
@@ -6,7 +6,7 @@
 /*   By: mel-bakh <mel-bakh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/07 00:56:45 by mel-bakh          #+#    #+#             */
-/*   Updated: 2026/02/24 00:32:21 by mel-bakh         ###   ########.fr       */
+/*   Updated: 2026/02/25 02:26:15 by mel-bakh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -46,36 +46,49 @@ void	sort_three(t_stack **stack)
 	if ((*stack)->value > (*stack)->next->value)
 		sa(stack);
 }
+static int	get_min_value(t_stack *stack)
+{
+	int	min;
+
+	min = stack->value;
+	while (stack)
+	{
+		if (stack->value < min)
+			min = stack->value;
+		stack = stack->next;
+	}
+	return (min);
+}
 
 void	sort_four(t_stack **stack_a, t_stack **stack_b)
 {
 	int	size;
 	int	min_pos;
+	int	min_value;
 
 	ft_index_stack(stack_a);
 	assign_positions(*stack_a);
-	min_pos = get_lowest_pos(*stack_a);
+
 	size = stack_size(*stack_a);
+	min_pos = get_lowest_pos(*stack_a);
+	min_value = get_min_value(*stack_a);
+
 	if (min_pos <= size / 2)
 	{
-		while (min_pos > 0)
-		{
+		while ((*stack_a)->value != min_value)
 			ra(stack_a);
-			min_pos--;
-		}
 	}
 	else
 	{
-		while (min_pos < size)
-		{
+		while ((*stack_a)->value != min_value)
 			rra(stack_a);
-			min_pos++;
-		}
 	}
+
 	pb(stack_a, stack_b);
 	sort_three(stack_a);
 	pa(stack_a, stack_b);
 }
+
 
 void	sort_five(t_stack **stack_a, t_stack **stack_b)
 {
